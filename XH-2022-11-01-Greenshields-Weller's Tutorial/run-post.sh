0f60cfaab02030583b1b5138d926b3ee1a5f771d
Xuanming Huang <112839307+Goldeninscriptions@users.noreply.github.com>
2022-11-03 10:57:42 +0000

    HXM: move them into "2022-10-29"

--- XH-2022-11-01-Greenshields-Weller's Tutorial/run-post.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#BSUB -J gw_fsi_postprocess2
-#BSUB -q debug
-#BSUB -n 40 
-#BSUB -e %J.err
-#BSUB -o %J.out
-#BSUB -R "span[ptile=40]"
-
-mpirun -np 10 ../build/vis_fluid -ref true -time_start 0 -time_step 500 -time_end 2000
